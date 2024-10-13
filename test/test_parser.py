f5af040d73431cebec181c1349b2244a23634e7d
Zelfior <thibault.moulignier@hotmail.fr>
2024-10-13 17:43:47 +0200

    test base no longer need to know if has vfx

--- test/test_parser.py
+++ test/test_parser.py
@@ -3,6 +3,7 @@ import glob
 import os
 from pathlib import Path
 import sys
+import struct
 
 package_path = Path(__file__).absolute().parent.parent
 sys.path.append((package_path/"src").name)
@@ -11,7 +12,7 @@ from cs2_parsed_io import Cs2File
 from io_elementary import IOOperation
 
 
-def run_test(file_path, has_vfx = False):
+def run_test(file_path):
 
     if not os.path.exists(file_path):
         print(file_path)
@@ -25,11 +26,20 @@ def run_test(file_path, has_vfx = False):
     os.makedirs("garbage", exist_ok=True)
 
     cs2 = Cs2File.new_cs2file()
-    cs2.read_write_file(input_path.absolute(), 
-                            IOOperation.READ, has_vfx=has_vfx)
+
+    has_ = True
+
+    try:
+        cs2.read_write_file(input_path.absolute(), 
+                                IOOperation.READ, has_vfx=True)
+    except struct.error:
+        cs2.read_write_file(input_path.absolute(), 
+                                IOOperation.READ, has_vfx=False)
+        has_ = False
+        
 
     cs2.read_write_file(file_name,   
-                            IOOperation.WRITE, has_vfx=has_vfx)
+                            IOOperation.WRITE, has_vfx=has_)
 
     with open(input_path.absolute(), "rb") as ref_file:
         with open(file_name, "rb") as output_file:
@@ -56,7 +66,7 @@ def run_test(file_path, has_vfx = False):
 def test_30_30_10_tech():
     file_path = package_path/"files/cs2_parsed/30_30_10/30_30_10_tech.cs2.parsed"
 
-    run_test(file_path.absolute(), has_vfx=True)
+    run_test(file_path.absolute())
 
 def test_arena_tech():
     file_path = package_path/"files/cs2_parsed/arena/arena_tech.cs2.parsed"
@@ -71,12 +81,12 @@ def test_athens_acropolis_tech():
 def test_attila_cliff_01_tech():
     file_path = package_path/"files/cs2_parsed/attila_cliff_01/attila_cliff_01_tech.cs2.parsed"
 
-    run_test(file_path.absolute(), has_vfx=True)
+    run_test(file_path.absolute())
 
 def test_banner01_tech():
     file_path = package_path/"files/cs2_parsed/banner01/banner01_tech.cs2.parsed"
 
-    run_test(file_path.absolute(), has_vfx=True)
+    run_test(file_path.absolute())
 
 def test_barbarian_fort_curved_bastion_tech():
     file_path = package_path/"files/cs2_parsed/barbarian_fort_curved_bastion/barbarian_fort_curved_bastion_tech.cs2.parsed"
@@ -86,47 +96,47 @@ def test_barbarian_fort_curved_bastion_tech():
 def test_bridge_stone_1_tech():
     file_path = package_path/"files/cs2_parsed/bridge_stone_1/bridge_stone_1_tech.cs2.parsed"
 
-    run_test(file_path.absolute(), has_vfx=True)
+    run_test(file_path.absolute())
 
 def test_crane_tech():
     file_path = package_path/"files/cs2_parsed/crane/crane_tech.cs2.parsed"
 
-    run_test(file_path.absolute(), has_vfx=True)
+    run_test(file_path.absolute())
 
 def test_eastern_villa_house01_tech():
     file_path = package_path/"files/cs2_parsed/eastern_villa_house01/eastern_villa_house01_tech.cs2.parsed"
 
-    run_test(file_path.absolute(), has_vfx=True)
+    run_test(file_path.absolute())
 
 def test_eastern_villa_stables_tech():
     file_path = package_path/"files/cs2_parsed/eastern_villa_stables/eastern_villa_stables_tech.cs2.parsed"
 
-    run_test(file_path.absolute(), has_vfx=True)
+    run_test(file_path.absolute())
 
 def test_western_villa_house02_tech():
     file_path = package_path/"files/cs2_parsed/western_villa_house02/western_villa_house02_tech.cs2.parsed"
 
-    run_test(file_path.absolute(), has_vfx=True)
+    run_test(file_path.absolute())
 
 def test_western_villa_stables_tech():
     file_path = package_path/"files/cs2_parsed/western_villa_stables/western_villa_stables_tech.cs2.parsed"
 
-    run_test(file_path.absolute(), has_vfx=True)
+    run_test(file_path.absolute())
 
 def test_western_villa_straight01_tech():
     file_path = package_path/"files/cs2_parsed/western_villa_straight01/western_villa_straight01_tech.cs2.parsed"
 
-    run_test(file_path.absolute(), has_vfx=True)
+    run_test(file_path.absolute())
 
 def test_western_villa_straight02_tech():
     file_path = package_path/"files/cs2_parsed/western_villa_straight02/western_villa_straight02_tech.cs2.parsed"
 
-    run_test(file_path.absolute(), has_vfx=True)
+    run_test(file_path.absolute())
 
 def test_western_villa_tabernae_tech():
     file_path = package_path/"files/cs2_parsed/western_villa_tabernae/western_villa_tabernae_tech.cs2.parsed"
 
-    run_test(file_path.absolute(), has_vfx=True)
+    run_test(file_path.absolute())
 
 if __name__ == "__main__":
     test_athens_acropolis_tech()
